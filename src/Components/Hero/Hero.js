47c80e7205c5df49f2cef56e7b670e659dda7139
clencyc <clency2023@gmail.com>
2024-08-31 18:00:00 +0300

    changed UI

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../Hero/Hero.css'
-import heroImage from '../Assests/hero.jpg'
+import heroImage from '../Assests/pexels-pixabay-356040.jpg'
 // import { GiQueenCrown } from "react-icons/gi";
 
 function Hero() {
@@ -8,8 +8,8 @@ function Hero() {
     <div className='container-home' id='home'>
         <img src={heroImage} alt="" className='heroimage'/>
         <div className="herotext">
-            <h1>Ajiri<span>4</span>Her</h1>
-             <p className="tagline" id='tagline'> Connect,Learn,<span>Grow</span></p>
+            <h1>MedicLocate</h1>
+             <p className="tagline" id='tagline'> Locate, Get service<span>Be Informed</span></p>
         </div>
        
     </div>
