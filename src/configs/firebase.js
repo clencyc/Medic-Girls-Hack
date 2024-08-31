f8935f49d9220db283a2ecc389e15513862fd178
DevFaith <waitherafaith@outlook.com>
2024-08-31 18:11:21 +0300

    * chore(firebase.js): remove unused imports and comments, format code for better readability

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,18 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-
-
-// import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC7VBJuFcMNlmKFzuQnkirFNHE2pdzq9ik",
   authDomain: "codequeens-26324.firebaseapp.com",
@@ -23,11 +13,10 @@ const firebaseConfig = {
   measurementId: "G-3E3XZFQBD2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { db, auth, provider, storage};
+export { db, auth, provider, storage };
